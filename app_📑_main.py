061e57f0d8fa941f4c37121aa8483feb2838097b
Carlos Spoerer <105332088+caspoerer@users.noreply.github.com>
2022-12-19 21:03:36 -0300

    Update app_📑_main.py

--- app_📑_main.py
+++ app_📑_main.py
@@ -16,8 +16,8 @@ model_trainnig = st.container()
 with header:
     st.title("Hites: Fuga de Clientes")
     #col1, col2 = st.columns([1,1])
-    st.markdown("##A través de esta aplicación podrás monitorear la fuga de clientes")
-    st.markdown("Te mostraremos paso a pase el porqué los clientes se fugan y te entregaremos una recomendación para la retencion")
+    st.markdown("## A través de esta aplicación podrás monitorear la fuga de clientes")
+    st.markdown("### Te mostraremos paso a pase el porqué los clientes se fugan y te entregaremos una recomendación para la retencion")
     col1, col2 = st.columns([1,1])
     
 
